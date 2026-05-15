2e063036a05f11454c9ccd1ffcb2bf2792cb1288
bulskov <bulskov@ruc.dk>
2026-05-15 03:59:58 +0200

    build: skip cmake configure if cache exists

--- build.bat
+++ build.bat
@@ -4,10 +4,9 @@ setlocal
 if "%BUILD_DIR%"=="" set BUILD_DIR=build
 if "%BUILD_TYPE%"=="" set BUILD_TYPE=Debug
 
-if exist "%BUILD_DIR%\CMakeCache.txt" del /f /q "%BUILD_DIR%\CMakeCache.txt"
-if exist "%BUILD_DIR%\CMakeFiles" rmdir /s /q "%BUILD_DIR%\CMakeFiles"
-
-cmake -S . -B "%BUILD_DIR%"
-if errorlevel 1 exit /b 1
+if not exist "%BUILD_DIR%\CMakeCache.txt" (
+    cmake -S . -B "%BUILD_DIR%"
+    if errorlevel 1 exit /b 1
+)
 
 cmake --build "%BUILD_DIR%" --config %BUILD_TYPE% --parallel %NUMBER_OF_PROCESSORS%
